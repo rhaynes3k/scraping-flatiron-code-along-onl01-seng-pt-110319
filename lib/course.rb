5edcd16f47f52aac5d96c5b48041a49f32b898a2
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-02 22:41:46 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -4,9 +4,9 @@ class Course
     @@all = []
     
     def initialize(title, schedule, description)
-      @title = title
-      @schedule = schedule
-      @description = description
+      # @title = title
+      # @schedule = schedule
+      # @description = description
       @@all << self
     end
   
