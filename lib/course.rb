efb0c810c6a950b0d5d430ef6323cc6fff4d6296
Rommel Haynes <rhaynes3k@gmail.com>
2020-01-02 22:39:01 +0000

    Automatically backed up by Learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,11 +1,23 @@
 class Course
   attr_accessor :title, :schedule, :description
   
+    @@all = []
+    
+    def initialize(title, schedule, description)
+      @title = title
+      @schedule = schedule
+      @description = description
+    end
   
+    def self.all
+      @@all
+    end
   
-  
-  
-  
+    def reset.all
+      @@all = []
+    end
+    
+    
   
   
   
